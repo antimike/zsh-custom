ac09927ccb8c959077b5e74539c52f265b569815
Michael Haynes <antihaynes@gmail.com>
2022-03-14 00:18:16 -0500

    Add env file for man to enable custom mansects

---
+++ env/man.zsh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+# Config for personal "man sections"
+# Sections:
+#       1h      Manpages generated from command helptext using external tools
+#       1n      Personal notes / summaries
+
+manpath+=(/home/hactar/.local/share/man)
+export MANSECT="1:1p:8:2:3:3p:3pm:4:5:6:7:9:0p:n:l:p:o:1x:2x:3x:4x:5x:6x:7x:8x:1h:1n"
