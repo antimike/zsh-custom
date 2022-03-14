d86b830f8ba5a2e5796f8b73db0e6ba94c21467f
Michael Haynes <antihaynes@gmail.com>
2022-03-14 00:17:31 -0500

    Add TWF widget (file explorer)

---
+++ widgets/twf.zsh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+twf-widget() {
+  local selected=$(twf --height=0.5)
+  BUFFER="$BUFFER$selected"
+  zle reset-prompt
+  zle end-of-line
+  return $ret
+}
+zle -N twf-widget
+bindkey '^[T' twf-widget
